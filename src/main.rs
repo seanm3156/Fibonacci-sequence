ecaa39303c28fbad9a21bd33d86008853a74e613
Sean Mckeown <seanm3156@gmail.com>
2022-11-22 22:47:16 +0000

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::io;
 
 fn main() {
+    println!("Enter the number in the fibonacci sequence to calculate:");
+
     let mut n = String::new(); 
     io::stdin().read_line(&mut n).expect("Failed to readline");
     let n: u32 = n.trim().parse().expect("input a number!");
 
     let result = fibonacci(n);
-    println!("number {n} in the fibonacci sequence is {result}");
+    println!("\nNumber {n} in the fibonacci sequence is {result}");
 }
 
 fn fibonacci(n : u32) -> u32 {
