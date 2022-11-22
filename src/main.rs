b1180f5cc6e1d9aee08260d093627a95f33a4ddc
Sean Mckeown <seanm3156@gmail.com>
2022-11-22 22:39:26 +0000

    first commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+
+fn main() {
+    let mut n = String::new(); 
+    io::stdin().read_line(&mut n).expect("Failed to readline");
+    let n: u32 = n.trim().parse().expect("input a number!");
+
+    let result = fibonacci(n);
+    println!("number {n} in the fibonacci sequence is {result}");
+}
+
+fn fibonacci(n : u32) -> u32 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
